e8a62a6a4cadb99a7e51f98ad76fd0d3c1954c95
Lukáš Děkanovský <Lukas.Dekanovsky@advacam.cz>
2024-06-14 20:03:25 +0200

    v1.20 - Energy scan functionality

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import tkinter as tk
-from tkinter.ttk import Labelframe
 import os
 from tkinter import messagebox
 from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import Listbox, Checkbutton
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
-from ttkthemes import ThemedStyle
 import matplotlib
 
 from main_page import MainPage
@@ -63,16 +59,16 @@ def main():
 
     # WINDOW + CANVAS initialization
     window = tk.Tk()
-    style = ttk.Style()
+    # style = tk.Style()
     canvas = tk.Canvas(width=430, height=250, bg=WINDOW_BACKGROUND, highlightthickness=0)
     title_img = tk.PhotoImage(file="./images/title.png")
 
     # Configure the style
-    style.configure("Custom.TButton",
-                foreground="black",
-                background="white",
-                font=(FONT_NAME, 10, "bold"),
-                padding=2)
+    # style.configure("Custom.TButton",
+    #             foreground="black",
+    #             background="white",
+    #             font=(FONT_NAME, 10, "bold"),
+    #             padding=2)
 
     # --------------- MENU ----------------- #
     menu = Menu(window)
@@ -102,35 +98,35 @@ def main():
     scan_data_page = ScanDataProcessing(number_of_gifs=len(os.listdir(GIF_RESULTS_FOLDER)))
 
     # LABELS ----------------- #
-    data_processing2D_label = ttk.Label(text="2D Data processing", style="TLabel", font=(FONT_NAME, 15, "bold"), background=WINDOW_BACKGROUND)
+    data_processing2D_label = tk.Label(text="2D Data processing", font=(FONT_NAME, 15, "bold"), background=WINDOW_BACKGROUND)
     data_processing2D_label.grid(column=0, row=1, sticky="ew", padx=50)
 
-    open_file_button_label = ttk.Label(text="Select a source\ndata file", style="TLabel", font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
+    open_file_button_label = tk.Label(text="Select a source\ndata file", font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
     open_file_button_label.grid(column=0, row=2, sticky="w")
 
-    data_processing_label = ttk.Label(text="Data processing", font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
+    data_processing_label = tk.Label(text="Data processing", font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
     data_processing_label.grid(column=0, row=3, sticky="w")
 
-    data_processingCT_label = ttk.Label(text="Energy scanning", font=(FONT_NAME, 15, "bold"), background=WINDOW_BACKGROUND)
+    data_processingCT_label = tk.Label(text="Energy scanning", font=(FONT_NAME, 15, "bold"), background=WINDOW_BACKGROUND)
     data_processingCT_label.grid(column=0, row=4, sticky="ew", padx=50, pady=10)
 
-    open_files_button_label = ttk.Label(text="Select files for\ngif creation",font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
+    open_files_button_label = tk.Label(text="Select files for\ngif creation",font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
     open_files_button_label.grid(column=0, row=5, sticky="w")
 
-    create_gif_button_label = ttk.Label(text="Create a .gif file",font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
+    create_gif_button_label = tk.Label(text="Create a .gif file",font=(FONT_NAME, 10, "bold"), background=WINDOW_BACKGROUND)
     create_gif_button_label.grid(column=0, row=6, sticky="w")
 
     # BUTTONS ---------------- 
-    open_file_button = ttk.Button(style="Custom.TButton", text="Open file", command=lambda: open_file(window, homepage))
+    open_file_button = tk.Button(text="Open file", command=lambda: open_file(window, homepage))
     open_file_button.grid(column=0, row=2, sticky="e", padx=10)  
 
-    open_files_button = ttk.Button(style="Custom.TButton", text="Open files", command=lambda: open_files(window, homepage))
+    open_files_button = tk.Button(text="Open files", command=lambda: open_files(window, homepage))
     open_files_button.grid(column=0, row=5, sticky="e", padx=10)
 
-    reset_gif_folder = ttk.Button(style="Custom.TButton", text="Reset gif data", command=lambda: homepage.reset_gif_folder(GIF_FOLDER))
+    reset_gif_folder = tk.Button(text="Reset gif data", command=lambda: homepage.reset_gif_folder(GIF_FOLDER))
     reset_gif_folder.grid(column=1, row=5, sticky="w", padx=150)
 
-    gif_button = ttk.Button(style="Custom.TButton", text="Process GIF secv.", command=lambda: plot_gif(window, scan_data_page))
+    gif_button = tk.Button(text="Process GIF secv.", command=lambda: plot_gif(window, scan_data_page))
     gif_button.grid(column=0, row=6, padx=10, sticky="e", pady=7)
 
     # FILEBOX ---------------- #
@@ -155,13 +151,13 @@ def main():
         file_box.insert(tk.END, file_name)
 
         # Create delete button for each file
-        display_button = ttk.Button(style="Custom.TButton", text="Show", command=lambda: data_check(window, scan_data_page, file_box, data_folder))
+        display_button = tk.Button(text="Show", command=lambda: data_check(window, scan_data_page, file_box, data_folder))
         display_button.grid(column=3, row=2, padx=5, sticky="n")
 
-        delete_button = ttk.Button(style="Custom.TButton", text="Delete", command=lambda: delete_file(window, homepage, file_box, data_folder))
+        delete_button = tk.Button(text="Delete", command=lambda: delete_file(window, homepage, file_box, data_folder))
         delete_button.grid(column=3, row=3, padx=5, sticky="s")
 
-        plot_button = ttk.Button(style="Custom.TButton", text="Show 2D Image", command=lambda: plot_2D_image(window, scan_data_page, file_box, data_folder))
+        plot_button = tk.Button(text="Show 2D Image", command=lambda: plot_2D_image(window, scan_data_page, file_box, data_folder))
         plot_button.grid(column=0, row=3, padx=10, sticky="e")
 
 
